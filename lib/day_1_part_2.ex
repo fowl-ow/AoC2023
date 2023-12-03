27e6b7e1c530c62f310cb6ada00b0e9e1730580e
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2023-12-03 20:15:08 +0100

    Cleanup

--- lib/day_1_part_2.ex
+++ lib/day_1_part_2.ex
@@ -1,49 +1,30 @@
-defmodule Day1Part2 do
-  def calibrate() do
+defmodule Test do
+  def cal do
+    pattern = ~r/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/
+
     File.stream!("input/day_1.txt")
-    |> Stream.map(&String.trim/1)
-    |> Enum.reduce(0, fn line, acc ->
-      acc + extract_number(line)
+    |> Stream.map(fn line ->
+      matches =
+        Regex.scan(pattern, line, capture: :all_but_first)
+        |> List.flatten()
+        |> Enum.map(fn
+          "one" -> "1"
+          "two" -> "2"
+          "three" -> "3"
+          "four" -> "4"
+          "five" -> "5"
+          "six" -> "6"
+          "seven" -> "7"
+          "eight" -> "8"
+          "nine" -> "9"
+          input -> input
+        end)
+
+      first = List.first(matches)
+      last = List.last(matches)
+
+      String.to_integer(first <> last)
     end)
-  end
-
-  def extract_number(line) do
-    line
-    |> String.split("")
-    |> Enum.reduce({[], []}, fn char, {a, b} ->
-      case Integer.parse(char) do
-        {num, _} -> {[num | a], []}
-        _ -> spell_number(b ++ [char], a)
-      end
-    end)
-    |> assemble_number
-  end
-
-  defp assemble_number({a, _b}) do
-    [last | _] = a
-    [first | _] = Enum.reverse(a)
-    String.to_integer("#{first}#{last}")
-  end
-
-  def spell_number(list, a) do
-    i =
-      case String.downcase(List.to_string(list)) do
-        ~r/.*\bone\b.*/ -> 1
-        ~r/.*\btwo\b.*/ -> 2
-        ~r/.*\bthree\b.*/ -> 3
-        ~r/.*\bfour\b.*/ -> 4
-        ~r/.*\bfive\b.*/ -> 5
-        ~r/.*\bsix\b.*/ -> 6
-        ~r/.*\bseven\b.*/ -> 7
-        ~r/.*\beight\b.*/ -> 8
-        ~r/.*\bnine\b.*/ -> 9
-        _ -> 0
-      end
-
-    if i != 0 do
-      {[i | a], []}
-    else
-      {a, list}
-    end
+    |> Enum.sum()
   end
 end
